5bb89fc16175e62180e8f85071ef9a994b17af99
yfqian <yfqian@dces.cn>
2023-04-25 17:40:05 +0800

    loadbalancer 负载均衡策略修改

--- mall4cloud-common/mall4cloud-common-cloud/src/main/java/com/mall4j/cloud/common/cloud/config/RestTemplateConfig.java
+++ mall4cloud-common/mall4cloud-common-cloud/src/main/java/com/mall4j/cloud/common/cloud/config/RestTemplateConfig.java
@@ -1,7 +1,9 @@
-package com.mall4j.cloud.common.config;
+package com.mall4j.cloud.common.cloud.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,12 +13,16 @@ import org.springframework.web.client.RestTemplate;
  * @date 2020/7/11
  */
 @Configuration
+@LoadBalancerClient(value = "mall4cloud-order", configuration = CustomLoadBalancerConfiguration.class)
+@Slf4j
 public class RestTemplateConfig {
-
+	// @Value("${spring.application.name}")
+	// private String value;
 	@Bean
 	@ConditionalOnMissingBean
 	@LoadBalanced
 	public RestTemplate restTemplate() {
+		// log.debug("spring.application.name: {}",value);
 		return new RestTemplate();
 	}
 
